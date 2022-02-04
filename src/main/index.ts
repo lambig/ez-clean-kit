8a60b0c373e42aa9ddce6c5f21e6d9b47a46ae6f
Lambig <ceolnua51@gmail.com>
2022-02-04 10:52:42 +0900

    defined entry point

---
+++ src/main/index.ts
@@ -0,0 +1,11 @@
+export * from "./domain/DomainObject";
+
+export * from "./domain/value/DomainPrimitive";
+export * from "./domain/value/ValueObject";
+export * from "./domain/entity/DomainEntity";
+export * from "./domain/collection/DomainObjects";
+export * from "./domain/entity/Aggregate";
+
+export * from "./domain/Id";
+
+export * from "./repository/Repository";
