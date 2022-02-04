e03d43607f92cf0050462f82280b86a803537179
Lambig <ceolnua51@gmail.com>
2022-02-04 11:58:22 +0900

    moved index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+export * from "./src/main/domain/DomainObject";
+
+export * from "./src/main/domain/value/DomainPrimitive";
+export * from "./src/main/domain/value/ValueObject";
+export * from "./src/main/domain/entity/DomainEntity";
+export * from "./src/main/domain/collection/DomainObjects";
+export * from "./src/main/domain/entity/Aggregate";
+
+export * from "./src/main/domain/Id";
+
+export * from "./src/main/repository/Repository";
